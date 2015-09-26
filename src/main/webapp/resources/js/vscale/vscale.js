14ce88066cce47accaf2ddb4d35cd123d3b6b600
raiedsiddiqui <raiedsiddiqui@Raieds-MacBook-Pro.local>
2015-09-26 19:01:09 -0400

    Added JS for McGill VIA survey

--- src/main/webapp/resources/js/vscale/vscale.js
+++ src/main/webapp/resources/js/vscale/vscale.js
@@ -13,5 +13,11 @@ $(document).ready(function(){
 	if ($("#qtext:contains(How happy do you feel at the moment?)").length) {
 		$("#externalsurveycode").load("../resources/js/vscale/carerqolvscale.html");
 	}
+
+	//VIA (McGill)
+	var via = $("#qID").val()
+	if (via.indexOf("VIA") != -1) {
+		$("#externalsurveycode").load("../resources/js/vscale/VIA.html");
+	}
 	
 });
