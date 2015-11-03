65200e651281223779c7b1961e7166154144f4c5
Linda Xie <lindaxie2006@gmail.com>
2015-11-03 14:57:05 -0500

    Added missing class...

---
+++ src/main/java/org/tapestry/objects/AdminActivity.java
@@ -0,0 +1,73 @@
+package org.tapestry.objects;
+
+public class AdminActivity {
+	private int activityId;	
+	private String description;
+	private String date;
+	private String time;
+	private String startTime;
+	private String endTime;	
+	private int user;	
+	private String userName;	
+	private int siteId;
+	
+	public AdminActivity(){
+	}
+	
+	public int getActivityId() {
+		return activityId;
+	}
+	public void setActivityId(int activityId) {
+		this.activityId = activityId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public int getUser() {
+		return user;
+	}
+	public void setUser(int user) {
+		this.user = user;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getSiteId() {
+		return siteId;
+	}
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}	
+	
+	
+}
